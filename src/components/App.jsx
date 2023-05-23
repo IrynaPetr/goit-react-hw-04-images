4431ff745ab767f04d37bbaea752d8bf51a2f4af
IrynaPetr <iryna.bieliakova@gmail.com>
2023-05-23 17:52:53 +0300

    fixed loadmore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ useEffect(() => {
   const gatApiByName = async (input, page) => {
     try {
       setIsLoading(true);
-      setPictures([]);
+      // setPictures([]);
       const data = await getDataByName(input, page);
      setIsLoading(false);
     const arr = data.hits;
